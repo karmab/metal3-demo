5e6fe9a5c1ee3aad6baa366d97076def5109e3e0
karmab <karimboumedhel@gmail.com>
2019-06-18 21:50:03 +0200

    conditional install

--- administrator/deploy.sh
+++ administrator/deploy.sh
@@ -1,6 +1,19 @@
 sh /root/kubernetes.sh
+{% if kubevirt %}
 sh /root/kubevirt.sh
+{% endif %}
+{% if cdi %}
 sh /root/cdi.sh
+{% endif %}
+{% if rook %}
 sh /root/rook.sh
+{% endif %}
+{% if istio %}
 sh /root/istio.sh
+{% if knative %}
 sh /root/knative.sh
+{% endif %}
+{% endif %}
+{% if kafka %}
+sh /root/kafka.sh
+{% endif %}
