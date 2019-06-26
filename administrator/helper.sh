341c4736db8567e03863eb8a092fb8afeeb4ad2b
karmab <karimboumedhel@gmail.com>
2019-06-26 10:02:54 +0200

    simplify ironic script

--- administrator/helper.sh
+++ administrator/helper.sh
@@ -1,11 +1,11 @@
-alias kcontextlist="kubectl config view -o jsonpath='{.contexts[*].name}' | tr ' ' '\n'"
-alias kcontext="kubectl config current-context"
-alias kns="kubectl config get-contexts `kubectl config current-context`"
+alias k8scontextlist="kubectl config view -o jsonpath='{.contexts[*].name}' | tr ' ' '\n'"
+alias k8scontext="kubectl config current-context"
+alias k8sns="kubectl config get-contexts `kubectl config current-context`"
 
-function kcontextswitch {
+function k8sswitchcontext {
     kubectl config use-context $1
 }
-function kswitchns {
+function k8sswitchns {
     kubectl config set-context `kubectl config current-context` --namespace=$1
 }
 export OS_TOKEN=fake-token
--- administrator/ironic.sh
+++ administrator/ironic.sh
@@ -1,16 +1,10 @@
-WORKING_DIR=${WORKING_DIR:-"/opt/metal3-dev-env"}
-export IRONIC_IMAGE=${IRONIC_IMAGE:-"quay.io/metal3-io/ironic:master"}
-export IRONIC_INSPECTOR_IMAGE=${IRONIC_INSPECTOR_IMAGE:-"quay.io/metal3-io/ironic-inspector"}
-export IRONIC_DATA_DIR="$WORKING_DIR/ironic"
-
-provisioning_interface=eth1
-echo """DEVICE=$provisioning_interface
+echo """DEVICE=eth1
 ONBOOT=yes
 NM_CONTROLLED=no
 BOOTPROTO=static
 IPADDR=172.22.0.1
-NETMASK=255.255.255.0""" > /etc/sysconfig/network-scripts/ifcfg-$provisioning_interface
-ifup $provisioning_interface
+NETMASK=255.255.255.0""" > /etc/sysconfig/network-scripts/ifcfg-eth1
+ifup eth1
 
 yum -y install centos-release-openstack-stein.noarch
 yum -y install python2-openstackclient python2-ironicclient python-virtualbmc ipmitool
@@ -18,23 +12,21 @@ systemctl enable --now virtualbmc
 ssh-keyscan -H 192.168.122.1 >> ~/.ssh/known_hosts
 vbmc add kni-node01 --port 6230 --username admin --password admin --libvirt-uri qemu+ssh://root@192.168.122.1/system
 vbmc start kni-node01
-mkdir $WORKING_DIR
-chown root:root $WORKING_DIR
-chmod 755 $WORKING_DIR
-mkdir -p "$IRONIC_DATA_DIR/html/images"
-pushd "$IRONIC_DATA_DIR/html/images"
+mkdir /opt/metal3-dev-env
+chown root:root /opt/metal3-dev-env
+chmod 755 /opt/metal3-dev-env
+mkdir -p "/opt/metal3-dev-env/html/images"
+pushd "/opt/metal3-dev-env/html/images"
 curl --insecure --compressed -L https://images.rdoproject.org/master/rdo_trunk/current-tripleo-rdo/ironic-python-agent.tar | tar -xf -
-CENTOS_IMAGE=CentOS-7-x86_64-GenericCloud-1901.qcow2
-curl --insecure --compressed -O -L http://cloud.centos.org/centos/7/images/${CENTOS_IMAGE}
-curl --insecure --compressed -O -L http://cloud.centos.org/centos/7/images/${CENTOS_IMAGE}.md5sum
+curl --insecure --compressed -O -L http://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud-1901.qcow2
+curl --insecure --compressed -O -L http://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud-1901.qcow2.md5sum
 popd
 
 mariadb_password=$(echo $(date;hostname)|sha256sum |cut -c-20)
-#podman pod create -n ironic-pod 
-mkdir -p $IRONIC_DATA_DIR
-docker run -d --net host --privileged --name dnsmasq -v $IRONIC_DATA_DIR:/shared --entrypoint /bin/rundnsmasq --env PROVISIONING_INTERFACE=$provisioning_interface --env DNSMASQ_EXCEPT_INTERFACE=eth0 ${IRONIC_IMAGE}
-docker run -d --net host --privileged --name httpd -v $IRONIC_DATA_DIR:/shared --entrypoint /bin/runhttpd ${IRONIC_IMAGE}
-docker run -d --net host --privileged --name mariadb -v $IRONIC_DATA_DIR:/shared --entrypoint /bin/runmariadb -e MARIADB_PASSWORD=$mariadb_password ${IRONIC_IMAGE}
-docker run -d --net host --privileged --name ironic -e MARIADB_PASSWORD=$mariadb_password -v $IRONIC_DATA_DIR:/shared -e PROVISIONING_INTERFACE=$provisioning_interface ${IRONIC_IMAGE}
-docker run -d --net host --privileged --name ironic-inspector -e PROVISIONING_INTERFACE=$provisioning_interface ${IRONIC_INSPECTOR_IMAGE}
-#sed -i s@http://:80@http://172.22.0.1:80@ $WORKING_DIR/ironic/html/inspector.ipxe
+mkdir -p /opt/metal3-dev-env
+docker run -d --net host --privileged --name dnsmasq -v /opt/metal3-dev-env:/shared --entrypoint /bin/rundnsmasq --env eth1 --env DNSMASQ_EXCEPT_INTERFACE=eth0 quay.io/metal3-io/ironic:master
+docker run -d --net host --privileged --name httpd -v /opt/metal3-dev-env:/shared --entrypoint /bin/runhttpd quay.io/metal3-io/ironic:master
+docker run -d --net host --privileged --name mariadb -v /opt/metal3-dev-env:/shared --entrypoint /bin/runmariadb -e MARIADB_PASSWORD=$mariadb_password quay.io/metal3-io/ironic:master
+docker run -d --net host --privileged --name ironic -e MARIADB_PASSWORD=$mariadb_password -v /opt/metal3-dev-env:/shared -e PROVISIONING_INTERFACE=eth1 quay.io/metal3-io/ironic:master
+docker run -d --net host --privileged --name ironic-inspector -e PROVISIONING_INTERFACE=eth1 quay.io/metal3-io/ironic-inspector
+sed -i s@http://:80@http://172.22.0.1:80@ /opt/metal3-dev-env/ironic/html/inspector.ipxe
