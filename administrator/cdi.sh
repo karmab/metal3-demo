4eae14c74275b5512e336d87d660ed312fc2d61f
Alberto Losada <alosadag@redhat.com>
2019-10-21 16:23:37 +0200

    Fix cdi cr filename when installing CDI

--- administrator/cdi.sh
+++ administrator/cdi.sh
@@ -2,4 +2,4 @@ CDI_VERSION=${CDI_VERSION:-latest}
 [ "$CDI_VERSION" == "latest" ] && CDI_VERSION=$(curl -s https://api.github.com/repos/kubevirt/containerized-data-importer/releases|grep tag_name|sort -V | tail -1 | awk -F':' '{print $2}' | sed 's/,//' | xargs)
 export KUBECONFIG=/root/.kube/config
 kubectl create -f https://github.com/kubevirt/containerized-data-importer/releases/download/$CDI_VERSION/cdi-operator.yaml
-kubectl create -f https://github.com/kubevirt/containerized-data-importer/releases/download/$CDI_VERSION/cdi-operator-cr.yaml
+kubectl create -f https://github.com/kubevirt/containerized-data-importer/releases/download/$CDI_VERSION/cdi-cr.yaml
