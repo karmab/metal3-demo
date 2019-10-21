7c925484ad448483a8bae57fee3cbc8af87cd8ca
Alberto Losada <alosadag@redhat.com>
2019-10-21 16:28:25 +0200

    Fixing flannel yaml file when applying CNI plugin

--- administrator/kubernetes.sh
+++ administrator/kubernetes.sh
@@ -18,7 +18,7 @@ cp /etc/kubernetes/admin.conf /root/
 chown root:root /root/admin.conf
 export KUBECONFIG=/root/admin.conf
 kubectl taint nodes --all node-role.kubernetes.io/master-
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/62e44c867a2846fefb68bd5f178daf4da3095ccb/Documentation/kube-flannel.yml
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 mkdir -p /root/.kube
 cp -i /etc/kubernetes/admin.conf /root/.kube/config
 chown root:root /root/.kube/config
